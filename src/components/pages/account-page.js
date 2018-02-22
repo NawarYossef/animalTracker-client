4116735741a6cd170b32b111174abd0e9a996214
Nawar Yossef <nawaryossef2@gmail.com>
2018-02-22 00:15:24 -0500

    add input fields for address and shelter/clinic name

--- src/components/pages/account-page.js
+++ src/components/pages/account-page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import requiresLogin from "../requires-login";
 import { fetchProtectedData } from "../../actions/protected-data";
+import "./css/account-page.css";
 
 export class AccountPage extends React.Component {
   componentDidMount() {
@@ -11,7 +12,30 @@ export class AccountPage extends React.Component {
   render() {
     return (
       <div className="account">
-        <p>THIS IS THE ACCOUNT</p>
+        <h2>My ACCOUNT</h2>
+        <form>
+          <label>
+            <h3>USER NAME</h3>
+            <input type="text" name="first-name" />
+          </label>
+          <label>
+            <h3>FIRST NAME</h3>
+            <input type="text" name="first-name" />
+          </label>
+          <label>
+            <h3>LAST NAME</h3>
+            <input type="text" name="last-name" />
+          </label>
+          <label>
+            <h3>SHELTER/CLINIC NAME</h3>
+            <input type="text" name="shelter-name" />
+          </label>
+          <label>
+            <h3>ADDRESS</h3>
+            <input type="text" name="shelter-name" />
+          </label> 
+          <input type="submit" value="Submit" className="submit-button" />
+        </form>
       </div>
     );
   }
